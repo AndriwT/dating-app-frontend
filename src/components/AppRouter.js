15a9fd51b3a1159033c624b18408ef51df67d620
Andriw Tapanes <andriwtapanes@gmail.com>
2021-10-04 12:16:55 -0400

    created 404 page for path error managing

--- src/components/AppRouter.js
+++ src/components/AppRouter.js
@@ -1,10 +1,11 @@
 import { useContext } from "react";
-import { BrowserRouter, Route, Switch } from "react-router-dom";
+import { BrowserRouter, Redirect, Route, Switch } from "react-router-dom";
 import { UserContext } from "../context/UserContext";
 import AuthView from "../views/AuthView";
 import MatchView from "../views/MatchView";
 import SignupView from "../views/SignupView";
-import SideNavBar from "../components/SideNavBar";
+import SideNavBar from "./SideNavBar";
+import NotFoundPage from "./NotFoundPage";
 
 const AppRouter = () => {
   const { loggedIn } = useContext(UserContext);
@@ -25,6 +26,8 @@ const AppRouter = () => {
             <Route exact path="/matches" component={MatchView} />
           </>
         )}
+        <Route path="/404" component={NotFoundPage} />
+        <Redirect to="/404" />
       </Switch>
     </BrowserRouter>
   );
